36806aedc016481028e9033583576672d04375a7
Bangar Raju T <bangaraju.59@gmail.com>
2022-08-04 18:20:29 +0530

    Added override option to unzip files

--- mysql.sh
+++ mysql.sh
@@ -12,11 +12,11 @@
 
  mysql -uroot -pRoboShop@1
 
-# > uninstall plugin validate_password;
+#> uninstall plugin validate_password;
 
  curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
 
  cd /tmp
- unzip mysql.zip
+ unzip -o mysql.zip
  cd mysql-main
  mysql -u root -pRoboShop@1 <shipping.sql
