d276013bb714444d29794ba982b47124898fd4cb
Bangar Raju T <bangaraju.59@gmail.com>
2022-08-06 17:20:30 +0530

    Modified cart.sh file

--- cart.sh
+++ cart.sh
@@ -4,28 +4,28 @@ set -e
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
 
  echo Installing NodeJS
- yum install nodejs -y >>/tmp/cart.log
+ yum install nodejs -y &>>/tmp/cart.log
 
  echo Adding Applicatyion user
- useradd roboshop >>/tmp/cart.log
+ useradd roboshop &>>/tmp/cart.log
 
  echo Downloading Application content
  curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" >>/tmp/cart.log
- cd /home/roboshop >>/tmp/cart.log
+ cd /home/roboshop &>>/tmp/cart.log
 
  echo Cleaning old application content
- rm -rf cart >>/tmp/cart.log
+ rm -rf cart &>>/tmp/cart.log
 
  echo Extracting Application archive
- unzip -o /tmp/cart.zip >>/tmp/cart.log
- mv cart-main cart >>/tmp/cart.log
- cd cart >>/tmp/cart.log
+ unzip -o /tmp/cart.zip &>>/tmp/cart.log
+ mv cart-main cart &>>/tmp/cart.log
+ cd cart &>>/tmp/cart.log
 
  echo Installing NodeJS dependencies
- npm install >>/tmp/cart.log
+ npm install &>>/tmp/cart.log
 
 echo Configuring card systemD service
- mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service >>/tmp/cart.log
- systemctl daemon-reload >>/tmp/cart.log
- systemctl start cart >>/tmp/cart.log
- systemctl enable cart >>/tmp/cart.log
+ mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
+ systemctl daemon-reload &>>/tmp/cart.log
+ systemctl start cart &>>/tmp/cart.log
+ systemctl enable cart &>>/tmp/cart.log
