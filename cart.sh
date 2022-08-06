f1a0c07344fa9718e0d77b5e7e6629051f37d4ac
Bangar Raju T <bangaraju.59@gmail.com>
2022-08-06 17:55:49 +0530

    Modified cart.sh file

--- cart.sh
+++ cart.sh
@@ -2,30 +2,78 @@
 
  echo Setting NodeJS repos
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
+ if [ $? -eq 0 ]
+ then
+   echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    fi
 
  echo Installing NodeJS
  yum install nodejs -y &>>/tmp/cart.log
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+   else
+     echo -e "\e[31mFAILURE\e[0m"
+     fi
 
  echo Adding Applicatyion user
  useradd roboshop &>>/tmp/cart.log
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+   else
+     echo -e "\e[31mFAILURE\e[0m"
+     fi
 
  echo Downloading Application content
  curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" >>/tmp/cart.log
  cd /home/roboshop &>>/tmp/cart.log
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+   else
+     echo -e "\e[31mFAILURE\e[0m"
+     fi
 
  echo Cleaning old application content
  rm -rf cart &>>/tmp/cart.log
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+   else
+     echo -e "\e[31mFAILURE\e[0m"
+     fi
 
  echo Extracting Application archive
  unzip -o /tmp/cart.zip &>>/tmp/cart.log
  mv cart-main cart &>>/tmp/cart.log
  cd cart &>>/tmp/cart.log
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+   else
+     echo -e "\e[31mFAILURE\e[0m"
+     fi
 
  echo Installing NodeJS dependencies
  npm install &>>/tmp/cart.log
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+   else
+     echo -e "\e[31mFAILURE\e[0m"
+     fi
 
 echo Configuring card systemD service
  mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
  systemctl daemon-reload &>>/tmp/cart.log
  systemctl start cart &>>/tmp/cart.log
  systemctl enable cart &>>/tmp/cart.log
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+   else
+     echo -e "\e[31mFAILURE\e[0m"
+     fi
